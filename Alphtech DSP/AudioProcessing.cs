5a76bdf065dca19130ff9590be39ef90df771b91
mikedevmen <dangmike1304@gmail.com>
2025-06-03 09:33:14 +0700

    Program foundation

---
+++ Alphtech DSP/AudioProcessing.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace AplhtechDSP
+{
+    public class AudioProcessing : IDisposable
+    {
+        private WaveInEvent input;
+        private WaveOutEvent output;
+        private BufferedWaveProvider buffer;
+
+        public AudioProcessing()
+        {
+            input = new WaveInEvent
+            {
+                WaveFormat = new WaveFormat(44100, 1),
+                BufferMilliseconds = 15
+            };
+
+            output = new WaveOutEvent
+            {
+                DesiredLatency = 50
+            };
+            buffer = new BufferedWaveProvider(input.WaveFormat)
+            {
+                DiscardOnBufferOverflow = true
+            };
+
+            input.DataAvailable += OnDataAvailable;
+            output.Init(buffer);
+        }
+
+        public void Start()
+        {
+            input.StartRecording();
+            output.Play();
+        }
+
+        public void Stop()
+        {
+            input.StopRecording();
+            output.Stop();
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            buffer.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        public void Dispose() //cleanup resources
+        {
+            input?.Dispose();
+            output?.Dispose();
+        }
+    }
+}
+
---
+++ Alphtech DSP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AplhtechDSP;
+
+class Program
+{
+    static void Main()
+    {
+        using (var amp = new AudioProcessing())
+        {
+            amp.Start();
+            Console.ReadLine();
+            amp.Stop();
+        }
+    }
+}
